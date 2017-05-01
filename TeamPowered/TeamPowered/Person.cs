3452cefc5904c6b3b1e1c9a599a5102f66c36ebe
An <An@AN-SURFACEPRO3>
2017-05-01 19:15:34 +0800

    Split name into first and last

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string Name { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
         public string Address { get; set; }
 
     }
